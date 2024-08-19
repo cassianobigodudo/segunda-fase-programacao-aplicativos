5e12552aa9aefcbfc19613e2773f4b3d643e95e9
cassianobigodudo <kakacassiano2010@hotmail.com>
2024-08-19 17:07:44 -0300

    Adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='tituloUm'>Hello React JS Fofinho</h1>
+      <p className='paragrafoUm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempore dolorem ea dolore, cum nihil cumque sed harum tempora reprehenderit ex facilis tenetur eligendi nobis quisquam molestias laudantium! Animi, pariatur!</p>
+    </>
+  )
+}
+
+export default App
